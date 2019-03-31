18015a03a48f3aedabefe5e061ec54121e36fe1d
Barker, Thomas (tb832f) <tb832f@att.com>
2019-03-31 10:38:26 -0600

    added missing files

---
+++ src/main/java/com/example/demo/entity/MyTest.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+public class MyTest {
+	private String name;
+	private String title;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
