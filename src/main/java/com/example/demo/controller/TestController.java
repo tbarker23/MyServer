c4b7aa5cd7d6f819f7ec9201dfea0b451b1c842d
tommybarker <tommybarker@10.0.0.86>
2019-03-31 20:55:43 -0600

    updated the test controller to return a test rest object for now

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,21 +1,21 @@
 package com.example.demo.controller;
 
-import java.io.File;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.MyTest;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 	
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.ALL_VALUE)
-	public ResponseEntity<File> test() {		
-		File file = new File("test.jpg");	
-		return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(file);
+	@RequestMapping(method = RequestMethod.GET)
+	public MyTest test() {		
+		MyTest test = new MyTest();
+		test.setName("tester mctestface");
+		test.setTitle("tester guy");
+		return test;
 	}
 	
 	
